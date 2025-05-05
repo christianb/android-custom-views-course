7b06079569c239bbb2f6aea8b86ceccbc4f0985d
Christian Bunk <376601+christianb@users.noreply.github.com>
2025-05-05 20:04:32 +0200

    finish Exercise 3

--- app/src/main/java/com/techyourchance/androidviews/exercises/_01_/MySliderFragment.kt
+++ app/src/main/java/com/techyourchance/androidviews/exercises/_01_/MySliderFragment.kt
@@ -4,21 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.techyourchance.androidviews.general.BaseFragment
 import com.techyourchance.androidviews.R
+import com.techyourchance.androidviews.exercises._03_.SliderChangeListener
 
-class MySliderFragment : BaseFragment() {
+class MySliderFragment : BaseFragment(), SliderChangeListener {
 
-    override val screenName get() = getString(R.string.screen_name_my_slider)
+	override val screenName get() = getString(R.string.screen_name_my_slider)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return layoutInflater.inflate(R.layout.layout_my_slider, container, false)
-    }
+	private lateinit var sliderText: TextView
+	private lateinit var mySlider: MySliderView
 
-    companion object {
-        fun newInstance(): MySliderFragment {
-            return MySliderFragment()
-        }
-    }
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+		return layoutInflater.inflate(R.layout.layout_my_slider, container, false)
+	}
 
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		sliderText = view.findViewById(R.id.slider_position)
+		mySlider = view.findViewById(R.id.my_slider)
+		mySlider.sliderChangeListener = this
+	}
+
+	override fun onValueChanged(value: Float) {
+		sliderText.text = value.toString()
+	}
+
+	override fun onStart() {
+		super.onStart()
+		sliderText.text = mySlider.value.toString()
+	}
+
+	companion object {
+		fun newInstance(): MySliderFragment {
+			return MySliderFragment()
+		}
+	}
 }
--- app/src/main/java/com/techyourchance/androidviews/exercises/_01_/MySliderView.kt
+++ app/src/main/java/com/techyourchance/androidviews/exercises/_01_/MySliderView.kt
@@ -9,100 +9,112 @@ import android.view.MotionEvent
 import androidx.core.content.ContextCompat
 import com.techyourchance.androidviews.CustomViewScaffold
 import com.techyourchance.androidviews.R
+import com.techyourchance.androidviews.exercises._03_.SliderChangeListener
 import com.techyourchance.androidviews.general.extensions.MotionEventExtensions.distanceTo
-import kotlin.math.sqrt
 
 class MySliderView : CustomViewScaffold {
 
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(
-        context,
-        attrs,
-        defStyleAttr,
-        defStyleRes
-    )
+	constructor(context: Context?) : super(context)
+	constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+	constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+	constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(
+		context,
+		attrs,
+		defStyleAttr,
+		defStyleRes
+	)
 
-    private val paint = Paint()
+	var sliderChangeListener: SliderChangeListener? = null
+	var value: Float = 0f
 
-    private var lineXLeft = 0f
-    private var lineXRight = 0f
-    private var lineY = 0f
-    private var lineHeight = 0f
+	private val paint = Paint()
 
-    private var circleX = 0f
-    private var circleY = 0f
-    private var circleRadius = 0f
+	private var lineXLeft = 0f
+	private var lineXRight = 0f
+	private var lineY = 0f
+	private var lineHeight = 0f
 
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
-        val lineMarginHorizontal = dpToPx(LINE_MARGIN_HORIZONTAL_DP)
+	private var circleX = 0f
+	private var circleY = 0f
+	private var circleRadius = 0f
 
-        lineXLeft = lineMarginHorizontal
-        lineXRight = w - lineMarginHorizontal
-        lineY = h * LINE_VERTICAL_POS_FRACTION
-        lineHeight = dpToPx(LINE_HEIGHT)
+	override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+		super.onSizeChanged(w, h, oldw, oldh)
+		val lineMarginHorizontal = dpToPx(LINE_MARGIN_HORIZONTAL_DP)
 
-        val lineLength = lineXRight - lineXLeft
+		lineXLeft = lineMarginHorizontal
+		lineXRight = w - lineMarginHorizontal
+		lineY = h * LINE_VERTICAL_POS_FRACTION
+		lineHeight = dpToPx(LINE_HEIGHT)
 
-        circleRadius = dpToPx(CIRCLE_RADIUS_DP)
-        circleX = lineLength * CIRCLE_X_POS_FRACTION + lineMarginHorizontal
-        circleY = lineY
-    }
+		val lineLength = lineXRight - lineXLeft
 
-    private var isDrag = false
-    private var lastEventX = 0f
+		circleRadius = dpToPx(CIRCLE_RADIUS_DP)
+		circleX = lineLength * CIRCLE_X_POS_FRACTION + lineMarginHorizontal
+		circleY = lineY
 
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        return if (event.action == MotionEvent.ACTION_DOWN) {
-            if (event.distanceTo(circleX, circleY) > circleRadius) return false
-            isDrag = true
-            lastEventX = event.x
-            true
-        } else if (isDrag && event.action == MotionEvent.ACTION_MOVE) {
-            val dx = event.x - lastEventX
-            if (circleX + dx < lineXLeft || circleX + dx > lineXRight) return true
-            circleX += dx
-            lastEventX = event.x
-            invalidate()
-            true
-        } else {
-            isDrag = false
-            false
-        }
-    }
+		updateSliderValue(circleX)
+	}
 
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        paint.color = Color.LTGRAY
-        paint.style = Paint.Style.STROKE
-        paint.strokeWidth = lineHeight
+	private var isDrag = false
+	private var lastEventX = 0f
 
-        canvas.drawLine(
-            /* startX = */ lineXLeft,
-            /* startY = */ lineY,
-            /* stopX = */ lineXRight,
-            /* stopY = */ lineY,
-            /* paint = */ paint
-        )
+	override fun onTouchEvent(event: MotionEvent): Boolean {
+		return if (event.action == MotionEvent.ACTION_DOWN) {
+			if (event.distanceTo(circleX, circleY) > circleRadius) return false
+			isDrag = true
+			lastEventX = event.x
+			true
+		} else if (isDrag && event.action == MotionEvent.ACTION_MOVE) {
+			val dx = event.x - lastEventX
+			if (circleX + dx < lineXLeft || circleX + dx > lineXRight) return true
+			circleX += dx
+			lastEventX = event.x
+			updateSliderValue(circleX)
+			invalidate()
+			true
+		} else {
+			isDrag = false
+			false
+		}
+	}
 
-        paint.color = ContextCompat.getColor(context, R.color.primary_variant)
-        paint.style = Paint.Style.FILL
+	private fun updateSliderValue(sliderXPos: Float) {
+		val lineLength = lineXRight - lineXLeft
+		value = (sliderXPos - lineXLeft) / lineLength
+		sliderChangeListener?.onValueChanged(value)
+	}
 
-        canvas.drawCircle(
-            /* cx = */ circleX,
-            /* cy = */ circleY,
-            /* radius = */ circleRadius,
-            /* paint = */ paint
-        )
-    }
+	override fun onDraw(canvas: Canvas) {
+		super.onDraw(canvas)
+		paint.color = Color.LTGRAY
+		paint.style = Paint.Style.STROKE
+		paint.strokeWidth = lineHeight
 
-    companion object {
-        private const val LINE_MARGIN_HORIZONTAL_DP = 20f
-        private const val LINE_VERTICAL_POS_FRACTION = 0.7f
-        private const val LINE_HEIGHT = 5f
-        private const val CIRCLE_RADIUS_DP = 15f
-        private const val CIRCLE_X_POS_FRACTION = 1f
-    }
+		canvas.drawLine(
+			/* startX = */ lineXLeft,
+			/* startY = */ lineY,
+			/* stopX = */ lineXRight,
+			/* stopY = */ lineY,
+			/* paint = */ paint
+		)
+
+		paint.color = ContextCompat.getColor(context, R.color.primary_variant)
+		paint.style = Paint.Style.FILL
+
+		canvas.drawCircle(
+			/* cx = */ circleX,
+			/* cy = */ circleY,
+			/* radius = */ circleRadius,
+			/* paint = */ paint
+		)
+	}
+
+	companion object {
+		private const val LINE_MARGIN_HORIZONTAL_DP = 20f
+		private const val LINE_VERTICAL_POS_FRACTION = 0.7f
+		private const val LINE_HEIGHT = 5f
+		private const val CIRCLE_RADIUS_DP = 15f
+		private const val CIRCLE_X_POS_FRACTION = 0.5f
+	}
 }
