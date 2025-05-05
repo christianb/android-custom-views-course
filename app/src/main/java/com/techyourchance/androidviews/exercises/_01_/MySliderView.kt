a3d9b715cb3fcfc487b4d6074248b1765db7b74b
Christian Bunk <376601+christianb@users.noreply.github.com>
2025-05-05 18:34:12 +0200

    finish Exercise 1

--- app/src/main/java/com/techyourchance/androidviews/exercises/_01_/MySliderView.kt
+++ app/src/main/java/com/techyourchance/androidviews/exercises/_01_/MySliderView.kt
@@ -1,8 +1,13 @@
 package com.techyourchance.androidviews.exercises._01_
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
+import androidx.core.content.ContextCompat
 import com.techyourchance.androidviews.CustomViewScaffold
+import com.techyourchance.androidviews.R
 
 class MySliderView : CustomViewScaffold {
 
@@ -16,4 +21,65 @@ class MySliderView : CustomViewScaffold {
         defStyleRes
     )
 
+    private val paint = Paint()
+
+    private var lineXLeft = 0f
+    private var lineXRight = 0f
+    private var lineY = 0f
+    private var lineHeight = 0f
+
+    private var circleX = 0f
+    private var circleY = 0f
+    private var circleRadius = 0f
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        val lineMarginHorizontal = dpToPx(LINE_MARGIN_HORIZONTAL_DP)
+
+        lineXLeft = lineMarginHorizontal
+        lineXRight = w - lineMarginHorizontal
+        lineY = h * LINE_VERTICAL_POS_FRACTION
+        lineHeight = dpToPx(LINE_HEIGHT)
+
+        val lineLength = lineXRight - lineXLeft
+
+        circleRadius = dpToPx(CIRCLE_RADIUS_DP)
+        circleX = lineLength * CIRCLE_X_POS_FRACTION + lineMarginHorizontal
+        circleY = lineY// + (lineHeight / 2)
+
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        paint.color = Color.LTGRAY
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = lineHeight
+
+        canvas.drawLine(
+            /* startX = */ lineXLeft,
+            /* startY = */ lineY,
+            /* stopX = */ lineXRight,
+            /* stopY = */ lineY,
+            /* paint = */ paint
+        )
+
+
+        paint.color = ContextCompat.getColor(context, R.color.primary_variant)
+        paint.style = Paint.Style.FILL
+
+        canvas.drawCircle(
+            /* cx = */ circleX,
+            /* cy = */ circleY,
+            /* radius = */ circleRadius,
+            /* paint = */ paint
+        )
+    }
+
+    companion object {
+        private const val LINE_MARGIN_HORIZONTAL_DP = 20f
+        private const val LINE_VERTICAL_POS_FRACTION = 0.7f
+        private const val LINE_HEIGHT = 5f
+        private const val CIRCLE_RADIUS_DP = 15f
+        private const val CIRCLE_X_POS_FRACTION = 1f
+    }
 }
